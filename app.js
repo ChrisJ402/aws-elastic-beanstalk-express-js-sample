1724c1df678152110169773e58b4548931efe0f9
Christopher John <Christopher.John-CIC.Uk@ibm.com>
2021-05-13 16:06:58 +0100

    updated app.js to reflect the pipeline tests

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! This is my first deployment via Amazon!'));
+app.get('/', (req, res) => res.send('Hello World! This is my first deployment via Amazon! Checking now that I have added a test to the pipleline'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
